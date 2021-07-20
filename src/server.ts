3cb37d396fe2dc0ab5ee341cf49fd7c8501870b6
Eduardo Almeida <duduf2005@hotmail.com>
2021-07-20 01:14:59 -0300

    remove typescript dependences

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-require('dotenv').config({
-  path: ".env"
-});
-
-app.listen(3333, process.env.APP_HOST, () => {
-  console.log(`Starting API Controller at ${process.env.APP_HOST}:3333`);
-});
