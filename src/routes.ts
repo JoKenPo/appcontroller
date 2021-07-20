afd1b1f5f579a30f744d70fd8dbc159f58032c1c
Eduardo Almeida <duduf2005@hotmail.com>
2021-07-19 23:53:02 -0300

    add proxy endpoints and .env file

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,31 @@
 const routes = require('express').Router();
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-routes.get('/api/ml', (req, res) => {
+require('dotenv').config({
+  path: ".env"
+});
+
+// Logging
+routes.use(morgan('dev'));
+
+routes.get('/info', (req, res, next) => {
+  res.send('This is a proxy controller which proxies to MercadoLibre API.');
+});
+
+// Proxy endpoints
+routes.use('/api/ml', (req, res, next) => {
   console.log(req.connection.remoteAddress);
   console.log(req.url);
   console.log(req.method);
 
-  return res.status(200).send();
-})
+  createProxyMiddleware({
+    target: process.env.APP_API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/api/ml`]: '',
+    },
+  })
+});
 
 module.exports = routes;
